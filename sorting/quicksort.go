5e92b095fa417671d11f9f36d08815e9bb07cc75
raymondgitonga <raymond.gitonga@saltpay.co>
2022-07-12 12:51:28 +0300

    Quick sort

--- sorting/quicksort.go
+++ sorting/quicksort.go
@@ -9,21 +9,29 @@ func QuickSort(arr []int, lowIndex int, highIndex int) []int {
 	leftPointer := lowIndex
 	rightPointer := highIndex
 
+	// While left pointer and right pointer are not in the same place
 	for leftPointer < rightPointer {
+
+		// While element in left pointer is smaller than pivot and right pointer hasn't caught up
 		for arr[leftPointer] <= pivot && leftPointer < rightPointer {
 			leftPointer++
 		}
 
+		// While element in right pointer is larger than pivot and left pointer hasn't caught up
 		for arr[rightPointer] >= pivot && leftPointer < rightPointer {
 			rightPointer--
 		}
 
+		// Swap elements in right and left pointer
 		swap(arr, leftPointer, rightPointer)
 	}
 
+	// Swap elements in both pointers position(In same position) with pivot(last element)
 	swap(arr, leftPointer, highIndex)
 
+	// Quick sort left array i.e from start of the array to index before where pivot has been inserted
 	QuickSort(arr, lowIndex, leftPointer-1)
+	// Quick sort right array i.e from index before where pivot has been inserted to end of the array
 	QuickSort(arr, leftPointer+1, highIndex)
 
 	return arr
